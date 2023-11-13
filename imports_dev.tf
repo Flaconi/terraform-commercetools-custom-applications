864fac160b5db9924e66f4c607364474993a42bb
alexander.miehe <alexander.miehe@flaconi.de>
2023-11-13 17:17:49 +0100

    * imports

--- imports_dev.tf
+++ imports_dev.tf
@@ -1,300 +1,285 @@
 
 import {
   to = module.ssm["ct-mc-custom-app-affiliate-config"].aws_ssm_parameter.this["application_id"]
-  id = "/lite/dev/eu-central-1/applications/ct-mc-custom-app-affiliate-config/application_id"
+  id = "/lite/stage/eu-central-1/applications/ct-mc-custom-app-affiliate-config/application_id"
 }
 
 import {
   to = module.ssm["ct-mc-custom-app-affiliate-config"].aws_ssm_parameter.this["bucket_name"]
-  id = "/lite/dev/eu-central-1/applications/ct-mc-custom-app-affiliate-config/bucket_name"
+  id = "/lite/stage/eu-central-1/applications/ct-mc-custom-app-affiliate-config/bucket_name"
 }
 
 import {
   to = module.ssm["ct-mc-custom-app-affiliate-config"].aws_ssm_parameter.this["cloudfront_domain_name"]
-  id = "/lite/dev/eu-central-1/applications/ct-mc-custom-app-affiliate-config/cloudfront_domain_name"
+  id = "/lite/stage/eu-central-1/applications/ct-mc-custom-app-affiliate-config/cloudfront_domain_name"
 }
 
 import {
   to = module.ssm["ct-mc-custom-app-competition"].aws_ssm_parameter.this["application_id"]
-  id = "/lite/dev/eu-central-1/applications/ct-mc-custom-app-competition/application_id"
+  id = "/lite/stage/eu-central-1/applications/ct-mc-custom-app-competition/application_id"
 }
 
 import {
   to = module.ssm["ct-mc-custom-app-competition"].aws_ssm_parameter.this["bucket_name"]
-  id = "/lite/dev/eu-central-1/applications/ct-mc-custom-app-competition/bucket_name"
+  id = "/lite/stage/eu-central-1/applications/ct-mc-custom-app-competition/bucket_name"
 }
 
 import {
   to = module.ssm["ct-mc-custom-app-competition"].aws_ssm_parameter.this["campaign_api_url"]
-  id = "/lite/dev/eu-central-1/applications/ct-mc-custom-app-competition/campaign_api_url"
+  id = "/lite/stage/eu-central-1/applications/ct-mc-custom-app-competition/campaign_api_url"
 }
 
 import {
   to = module.ssm["ct-mc-custom-app-competition"].aws_ssm_parameter.this["cloudfront_domain_name"]
-  id = "/lite/dev/eu-central-1/applications/ct-mc-custom-app-competition/cloudfront_domain_name"
+  id = "/lite/stage/eu-central-1/applications/ct-mc-custom-app-competition/cloudfront_domain_name"
 }
 
 import {
   to = module.ssm["ct-mc-custom-app-competition"].aws_ssm_parameter.this["cognito_auth_client_id"]
-  id = "/lite/dev/eu-central-1/applications/ct-mc-custom-app-competition/cognito_auth_client_id"
+  id = "/lite/stage/eu-central-1/applications/ct-mc-custom-app-competition/cognito_auth_client_id"
 }
 
 import {
   to = module.ssm["ct-mc-custom-app-competition"].aws_ssm_parameter.this["cognito_auth_client_secret"]
-  id = "/lite/dev/eu-central-1/applications/ct-mc-custom-app-competition/cognito_auth_client_secret"
+  id = "/lite/stage/eu-central-1/applications/ct-mc-custom-app-competition/cognito_auth_client_secret"
 }
 
 import {
   to = module.ssm["ct-mc-custom-app-competition"].aws_ssm_parameter.this["cognito_auth_url"]
-  id = "/lite/dev/eu-central-1/applications/ct-mc-custom-app-competition/cognito_auth_url"
+  id = "/lite/stage/eu-central-1/applications/ct-mc-custom-app-competition/cognito_auth_url"
 }
 
 import {
   to = module.ssm["ct-mc-custom-app-config"].aws_ssm_parameter.this["application_id"]
-  id = "/lite/dev/eu-central-1/applications/ct-mc-custom-app-config/application_id"
+  id = "/lite/stage/eu-central-1/applications/ct-mc-custom-app-config/application_id"
 }
 
 import {
   to = module.ssm["ct-mc-custom-app-config"].aws_ssm_parameter.this["bucket_name"]
-  id = "/lite/dev/eu-central-1/applications/ct-mc-custom-app-config/bucket_name"
+  id = "/lite/stage/eu-central-1/applications/ct-mc-custom-app-config/bucket_name"
 }
 
 import {
   to = module.ssm["ct-mc-custom-app-config"].aws_ssm_parameter.this["cloudfront_domain_name"]
-  id = "/lite/dev/eu-central-1/applications/ct-mc-custom-app-config/cloudfront_domain_name"
+  id = "/lite/stage/eu-central-1/applications/ct-mc-custom-app-config/cloudfront_domain_name"
 }
 
 import {
   to = module.ssm["ct-mc-custom-app-config"].aws_ssm_parameter.this["ct_cart_boxes_upload_bucket_access_key_id"]
-  id = "/lite/dev/eu-central-1/applications/ct-mc-custom-app-config/ct_cart_boxes_upload_bucket_access_key_id"
+  id = "/lite/stage/eu-central-1/applications/ct-mc-custom-app-config/ct_cart_boxes_upload_bucket_access_key_id"
 }
 
 import {
   to = module.ssm["ct-mc-custom-app-config"].aws_ssm_parameter.this["ct_cart_boxes_upload_bucket_access_key_secret"]
-  id = "/lite/dev/eu-central-1/applications/ct-mc-custom-app-config/ct_cart_boxes_upload_bucket_access_key_secret"
+  id = "/lite/stage/eu-central-1/applications/ct-mc-custom-app-config/ct_cart_boxes_upload_bucket_access_key_secret"
 }
 
 import {
   to = module.ssm["ct-mc-custom-app-config"].aws_ssm_parameter.this["ct_cart_boxes_upload_bucket_name"]
-  id = "/lite/dev/eu-central-1/applications/ct-mc-custom-app-config/ct_cart_boxes_upload_bucket_name"
-}
-
-import {
-  to = module.ssm["ct-mc-custom-app-developers"].aws_ssm_parameter.this["application_id"]
-  id = "/lite/dev/eu-central-1/applications/ct-mc-custom-app-developers/application_id"
-}
-
-import {
-  to = module.ssm["ct-mc-custom-app-developers"].aws_ssm_parameter.this["bucket_name"]
-  id = "/lite/dev/eu-central-1/applications/ct-mc-custom-app-developers/bucket_name"
-}
-
-import {
-  to = module.ssm["ct-mc-custom-app-developers"].aws_ssm_parameter.this["cloudfront_domain_name"]
-  id = "/lite/dev/eu-central-1/applications/ct-mc-custom-app-developers/cloudfront_domain_name"
+  id = "/lite/stage/eu-central-1/applications/ct-mc-custom-app-config/ct_cart_boxes_upload_bucket_name"
 }
 
 import {
   to = module.ssm["ct-mc-custom-app-discounts"].aws_ssm_parameter.this["application_id"]
-  id = "/lite/dev/eu-central-1/applications/ct-mc-custom-app-discounts/application_id"
+  id = "/lite/stage/eu-central-1/applications/ct-mc-custom-app-discounts/application_id"
 }
 
 import {
   to = module.ssm["ct-mc-custom-app-discounts"].aws_ssm_parameter.this["bucket_name"]
-  id = "/lite/dev/eu-central-1/applications/ct-mc-custom-app-discounts/bucket_name"
+  id = "/lite/stage/eu-central-1/applications/ct-mc-custom-app-discounts/bucket_name"
 }
 
 import {
   to = module.ssm["ct-mc-custom-app-discounts"].aws_ssm_parameter.this["cloudfront_domain_name"]
-  id = "/lite/dev/eu-central-1/applications/ct-mc-custom-app-discounts/cloudfront_domain_name"
+  id = "/lite/stage/eu-central-1/applications/ct-mc-custom-app-discounts/cloudfront_domain_name"
 }
 
 import {
   to = module.ssm["ct-mc-custom-app-internal-linking"].aws_ssm_parameter.this["application_id"]
-  id = "/lite/dev/eu-central-1/applications/ct-mc-custom-app-internal-linking/application_id"
+  id = "/lite/stage/eu-central-1/applications/ct-mc-custom-app-internal-linking/application_id"
 }
 
 import {
   to = module.ssm["ct-mc-custom-app-internal-linking"].aws_ssm_parameter.this["bucket_name"]
-  id = "/lite/dev/eu-central-1/applications/ct-mc-custom-app-internal-linking/bucket_name"
+  id = "/lite/stage/eu-central-1/applications/ct-mc-custom-app-internal-linking/bucket_name"
 }
 
 import {
   to = module.ssm["ct-mc-custom-app-internal-linking"].aws_ssm_parameter.this["cloudfront_domain_name"]
-  id = "/lite/dev/eu-central-1/applications/ct-mc-custom-app-internal-linking/cloudfront_domain_name"
+  id = "/lite/stage/eu-central-1/applications/ct-mc-custom-app-internal-linking/cloudfront_domain_name"
 }
 
 import {
   to = module.ssm["ct-mc-custom-app-internal-linking"].aws_ssm_parameter.this["ct_internal-linking_upload_bucket_access_key_id"]
-  id = "/lite/dev/eu-central-1/applications/ct-mc-custom-app-internal-linking/ct_internal-linking_upload_bucket_access_key_id"
+  id = "/lite/stage/eu-central-1/applications/ct-mc-custom-app-internal-linking/ct_internal-linking_upload_bucket_access_key_id"
 }
 
 import {
   to = module.ssm["ct-mc-custom-app-internal-linking"].aws_ssm_parameter.this["ct_internal-linking_upload_bucket_access_key_secret"]
-  id = "/lite/dev/eu-central-1/applications/ct-mc-custom-app-internal-linking/ct_internal-linking_upload_bucket_access_key_secret"
+  id = "/lite/stage/eu-central-1/applications/ct-mc-custom-app-internal-linking/ct_internal-linking_upload_bucket_access_key_secret"
 }
 
 import {
   to = module.ssm["ct-mc-custom-app-internal-linking"].aws_ssm_parameter.this["ct_internal-linking_upload_bucket_name"]
-  id = "/lite/dev/eu-central-1/applications/ct-mc-custom-app-internal-linking/ct_internal-linking_upload_bucket_name"
+  id = "/lite/stage/eu-central-1/applications/ct-mc-custom-app-internal-linking/ct_internal-linking_upload_bucket_name"
 }
 
 import {
   to = module.ssm["ct-mc-custom-app-pc-upload"].aws_ssm_parameter.this["application_id"]
-  id = "/lite/dev/eu-central-1/applications/ct-mc-custom-app-pc-upload/application_id"
+  id = "/lite/stage/eu-central-1/applications/ct-mc-custom-app-pc-upload/application_id"
 }
 
 import {
   to = module.ssm["ct-mc-custom-app-pc-upload"].aws_ssm_parameter.this["bucket_name"]
-  id = "/lite/dev/eu-central-1/applications/ct-mc-custom-app-pc-upload/bucket_name"
+  id = "/lite/stage/eu-central-1/applications/ct-mc-custom-app-pc-upload/bucket_name"
 }
 
 import {
   to = module.ssm["ct-mc-custom-app-pc-upload"].aws_ssm_parameter.this["cloudfront_domain_name"]
-  id = "/lite/dev/eu-central-1/applications/ct-mc-custom-app-pc-upload/cloudfront_domain_name"
+  id = "/lite/stage/eu-central-1/applications/ct-mc-custom-app-pc-upload/cloudfront_domain_name"
 }
 
 import {
   to = module.ssm["ct-mc-custom-app-pc-upload"].aws_ssm_parameter.this["ct_csv_upload_bucket_access_key_id"]
-  id = "/lite/dev/eu-central-1/applications/ct-mc-custom-app-pc-upload/ct_csv_upload_bucket_access_key_id"
+  id = "/lite/stage/eu-central-1/applications/ct-mc-custom-app-pc-upload/ct_csv_upload_bucket_access_key_id"
 }
 
 import {
   to = module.ssm["ct-mc-custom-app-pc-upload"].aws_ssm_parameter.this["ct_csv_upload_bucket_access_key_secret"]
-  id = "/lite/dev/eu-central-1/applications/ct-mc-custom-app-pc-upload/ct_csv_upload_bucket_access_key_secret"
+  id = "/lite/stage/eu-central-1/applications/ct-mc-custom-app-pc-upload/ct_csv_upload_bucket_access_key_secret"
 }
 
 import {
   to = module.ssm["ct-mc-custom-app-pc-upload"].aws_ssm_parameter.this["ct_csv_upload_bucket_name"]
-  id = "/lite/dev/eu-central-1/applications/ct-mc-custom-app-pc-upload/ct_csv_upload_bucket_name"
+  id = "/lite/stage/eu-central-1/applications/ct-mc-custom-app-pc-upload/ct_csv_upload_bucket_name"
 }
 
 import {
   to = module.ssm["ct-mc-custom-app-pim"].aws_ssm_parameter.this["application_id"]
-  id = "/lite/dev/eu-central-1/applications/ct-mc-custom-app-pim/application_id"
+  id = "/lite/stage/eu-central-1/applications/ct-mc-custom-app-pim/application_id"
 }
 
 import {
   to = module.ssm["ct-mc-custom-app-pim"].aws_ssm_parameter.this["bucket_name"]
-  id = "/lite/dev/eu-central-1/applications/ct-mc-custom-app-pim/bucket_name"
+  id = "/lite/stage/eu-central-1/applications/ct-mc-custom-app-pim/bucket_name"
 }
 
 import {
   to = module.ssm["ct-mc-custom-app-pim"].aws_ssm_parameter.this["cloudfront_domain_name"]
-  id = "/lite/dev/eu-central-1/applications/ct-mc-custom-app-pim/cloudfront_domain_name"
+  id = "/lite/stage/eu-central-1/applications/ct-mc-custom-app-pim/cloudfront_domain_name"
 }
 
 import {
   to = module.ssm["ct-mc-custom-app-pim"].aws_ssm_parameter.this["color_icon_access_key_id"]
-  id = "/lite/dev/eu-central-1/applications/ct-mc-custom-app-pim/color_icon_access_key_id"
+  id = "/lite/stage/eu-central-1/applications/ct-mc-custom-app-pim/color_icon_access_key_id"
 }
 
 import {
   to = module.ssm["ct-mc-custom-app-pim"].aws_ssm_parameter.this["color_icon_access_key_secret"]
-  id = "/lite/dev/eu-central-1/applications/ct-mc-custom-app-pim/color_icon_access_key_secret"
+  id = "/lite/stage/eu-central-1/applications/ct-mc-custom-app-pim/color_icon_access_key_secret"
 }
 
 import {
   to = module.ssm["ct-mc-custom-app-pim"].aws_ssm_parameter.this["color_icon_bucket_name"]
-  id = "/lite/dev/eu-central-1/applications/ct-mc-custom-app-pim/color_icon_bucket_name"
+  id = "/lite/stage/eu-central-1/applications/ct-mc-custom-app-pim/color_icon_bucket_name"
 }
 
 import {
   to = module.ssm["ct-mc-custom-app-pim"].aws_ssm_parameter.this["image_access_key_id"]
-  id = "/lite/dev/eu-central-1/applications/ct-mc-custom-app-pim/image_access_key_id"
+  id = "/lite/stage/eu-central-1/applications/ct-mc-custom-app-pim/image_access_key_id"
 }
 
 import {
   to = module.ssm["ct-mc-custom-app-pim"].aws_ssm_parameter.this["image_access_key_secret"]
-  id = "/lite/dev/eu-central-1/applications/ct-mc-custom-app-pim/image_access_key_secret"
+  id = "/lite/stage/eu-central-1/applications/ct-mc-custom-app-pim/image_access_key_secret"
 }
 
 import {
   to = module.ssm["ct-mc-custom-app-pim"].aws_ssm_parameter.this["image_bucket_name"]
-  id = "/lite/dev/eu-central-1/applications/ct-mc-custom-app-pim/image_bucket_name"
+  id = "/lite/stage/eu-central-1/applications/ct-mc-custom-app-pim/image_bucket_name"
 }
 
 import {
   to = module.ssm["ct-mc-custom-app-price-upload"].aws_ssm_parameter.this["application_id"]
-  id = "/lite/dev/eu-central-1/applications/ct-mc-custom-app-price-upload/application_id"
+  id = "/lite/stage/eu-central-1/applications/ct-mc-custom-app-price-upload/application_id"
 }
 
 import {
   to = module.ssm["ct-mc-custom-app-price-upload"].aws_ssm_parameter.this["bucket_name"]
-  id = "/lite/dev/eu-central-1/applications/ct-mc-custom-app-price-upload/bucket_name"
+  id = "/lite/stage/eu-central-1/applications/ct-mc-custom-app-price-upload/bucket_name"
 }
 
 import {
   to = module.ssm["ct-mc-custom-app-price-upload"].aws_ssm_parameter.this["cloudfront_domain_name"]
-  id = "/lite/dev/eu-central-1/applications/ct-mc-custom-app-price-upload/cloudfront_domain_name"
+  id = "/lite/stage/eu-central-1/applications/ct-mc-custom-app-price-upload/cloudfront_domain_name"
 }
 
 import {
   to = module.ssm["ct-mc-custom-app-price-upload"].aws_ssm_parameter.this["ct_price-upload_bucket_access_key_id"]
-  id = "/lite/dev/eu-central-1/applications/ct-mc-custom-app-price-upload/ct_price-upload_bucket_access_key_id"
+  id = "/lite/stage/eu-central-1/applications/ct-mc-custom-app-price-upload/ct_price-upload_bucket_access_key_id"
 }
 
 import {
   to = module.ssm["ct-mc-custom-app-price-upload"].aws_ssm_parameter.this["ct_price-upload_bucket_access_key_secret"]
-  id = "/lite/dev/eu-central-1/applications/ct-mc-custom-app-price-upload/ct_price-upload_bucket_access_key_secret"
+  id = "/lite/stage/eu-central-1/applications/ct-mc-custom-app-price-upload/ct_price-upload_bucket_access_key_secret"
 }
 
 import {
   to = module.ssm["ct-mc-custom-app-price-upload"].aws_ssm_parameter.this["ct_price-upload_bucket_name"]
-  id = "/lite/dev/eu-central-1/applications/ct-mc-custom-app-price-upload/ct_price-upload_bucket_name"
+  id = "/lite/stage/eu-central-1/applications/ct-mc-custom-app-price-upload/ct_price-upload_bucket_name"
 }
 
 import {
   to = module.ssm["ct-mc-custom-app-url-management"].aws_ssm_parameter.this["application_id"]
-  id = "/lite/dev/eu-central-1/applications/ct-mc-custom-app-url-management/application_id"
+  id = "/lite/stage/eu-central-1/applications/ct-mc-custom-app-url-management/application_id"
 }
 
 import {
   to = module.ssm["ct-mc-custom-app-url-management"].aws_ssm_parameter.this["bucket_name"]
-  id = "/lite/dev/eu-central-1/applications/ct-mc-custom-app-url-management/bucket_name"
+  id = "/lite/stage/eu-central-1/applications/ct-mc-custom-app-url-management/bucket_name"
 }
 
 import {
   to = module.ssm["ct-mc-custom-app-url-management"].aws_ssm_parameter.this["cloudfront_domain_name"]
-  id = "/lite/dev/eu-central-1/applications/ct-mc-custom-app-url-management/cloudfront_domain_name"
+  id = "/lite/stage/eu-central-1/applications/ct-mc-custom-app-url-management/cloudfront_domain_name"
 }
 
 import {
   to = module.ssm["ct-mc-custom-app-voucher"].aws_ssm_parameter.this["application_id"]
-  id = "/lite/dev/eu-central-1/applications/ct-mc-custom-app-voucher/application_id"
+  id = "/lite/stage/eu-central-1/applications/ct-mc-custom-app-voucher/application_id"
 }
 
 import {
   to = module.ssm["ct-mc-custom-app-voucher"].aws_ssm_parameter.this["bucket_name"]
-  id = "/lite/dev/eu-central-1/applications/ct-mc-custom-app-voucher/bucket_name"
+  id = "/lite/stage/eu-central-1/applications/ct-mc-custom-app-voucher/bucket_name"
 }
 
 import {
   to = module.ssm["ct-mc-custom-app-voucher"].aws_ssm_parameter.this["cloudfront_domain_name"]
-  id = "/lite/dev/eu-central-1/applications/ct-mc-custom-app-voucher/cloudfront_domain_name"
+  id = "/lite/stage/eu-central-1/applications/ct-mc-custom-app-voucher/cloudfront_domain_name"
 }
 
 import {
   to = module.ssm["ct-custom-app-subscribers"].aws_ssm_parameter.this["application_id"]
-  id = "/lite/dev/eu-central-1/applications/ct-custom-app-subscribers/application_id"
+  id = "/lite/stage/eu-central-1/applications/ct-custom-app-subscribers/application_id"
 }
 
 import {
   to = module.ssm["ct-custom-app-subscribers"].aws_ssm_parameter.this["bucket_name"]
-  id = "/lite/dev/eu-central-1/applications/ct-custom-app-subscribers/bucket_name"
+  id = "/lite/stage/eu-central-1/applications/ct-custom-app-subscribers/bucket_name"
 }
 
 import {
   to = module.ssm["ct-custom-app-subscribers"].aws_ssm_parameter.this["cloudfront_domain_name"]
-  id = "/lite/dev/eu-central-1/applications/ct-custom-app-subscribers/cloudfront_domain_name"
+  id = "/lite/stage/eu-central-1/applications/ct-custom-app-subscribers/cloudfront_domain_name"
 }
 
 import {
   to = module.ssm["ct-custom-app-subscribers"].aws_ssm_parameter.this["subscribers_api_client_id"]
-  id = "/lite/dev/eu-central-1/applications/ct-custom-app-subscribers/subscribers_api_client_id"
+  id = "/lite/stage/eu-central-1/applications/ct-custom-app-subscribers/subscribers_api_client_id"
 }
 
 import {
   to = module.ssm["ct-custom-app-subscribers"].aws_ssm_parameter.this["subscribers_api_client_secret"]
-  id = "/lite/dev/eu-central-1/applications/ct-custom-app-subscribers/subscribers_api_client_secret"
+  id = "/lite/stage/eu-central-1/applications/ct-custom-app-subscribers/subscribers_api_client_secret"
 }
